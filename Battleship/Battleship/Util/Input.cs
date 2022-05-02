99aa1c143dc520875280a6a884dacb9db1758476
Dienes Viktor <dienesviktor@gmail.com>
2022-05-02 14:03:30 +0200

    Implement Main method and main menu, and battleship class

---
+++ Battleship/Battleship/Util/Input.cs
@@ -0,0 +1,20 @@
+﻿namespace Battleship.Util
+{
+    public class Input
+    {
+        public string SelectMenu()
+        {
+            return Console.ReadLine();
+        }
+
+        public bool InputValidation(string input)
+        {
+            if (new int[] {1, 2, 3}.Contains(int.Parse(input)))
+            {
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
